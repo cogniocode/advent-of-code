43dcfb9fb2ee31903feffb33713f969a6ae7675d
Andrey Chervyakov <andrey@cognio.dev>
2022-12-06 19:50:03 +0600

    Add first task, part two

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,7 +1,7 @@
-use crate::puzzles::puzzle1::puzzle1_1;
+use crate::puzzles::puzzle1::{puzzle1_1, puzzle1_2};
 
 pub mod puzzles;
 
 fn main() {
-    puzzle1_1();
+    puzzle1_2();
 }
--- 2022/src/puzzles/puzzle1.rs
+++ 2022/src/puzzles/puzzle1.rs
@@ -22,3 +22,28 @@ pub fn puzzle1_1() {
 
     println!("{:?}", max_amount);
 }
+
+pub fn puzzle1_2() {
+    println!("{}", std::env::current_dir().unwrap().display());
+    let contents = std::fs::read_to_string(Path::new("./src/inputs/puzzle1.txt")).unwrap();
+    let bags = contents.split("\n\n").collect::<Vec<&str>>();
+
+    let mut total_bags = bags.iter()
+        .map(|v| {
+            let bag = *v;
+
+            let total_amount = bag
+                .split("\n")
+                .filter(|v| *v != "")
+                .map(|v| v.parse::<i32>().unwrap())
+                .reduce(|accum, v| accum + v)
+                .unwrap();
+
+            total_amount
+        })
+        .collect::<Vec<i32>>();
+
+    total_bags.sort_by(|a, b| b.cmp(a));
+
+    println!("{}", total_bags[0] + total_bags[1] + total_bags[2])
+}
